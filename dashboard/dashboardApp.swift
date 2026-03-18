11884163dea0546b1e412c366835e391468b84b8
Rayawa <171078364@qq.com>
2026-03-18 11:57:20 +0800

    Initial Commit

---
+++ dashboard/dashboardApp.swift
@@ -0,0 +1,17 @@
+//
+//  dashboardApp.swift
+//  dashboard
+//
+//  Created by Ray Chen on 2026.03.18.
+//
+
+import SwiftUI
+
+@main
+struct dashboardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
